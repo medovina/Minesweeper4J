94b1657e24a072b0cbdac40ea2544af07d1ae8f7
Adam Dingle <adam@medovina.org>
2020-01-16 18:04:58 +0100

    Add test() method in Evaluate class

--- Minesweeper4J-Playground/src/main/java/cz/minesweeper4j/playground/Evaluate.java
+++ Minesweeper4J-Playground/src/main/java/cz/minesweeper4j/playground/Evaluate.java
@@ -1,22 +1,17 @@
 package cz.minesweeper4j.playground;
 
-import java.io.FileNotFoundException;
-
 import cz.minesweeper4j.Minesweeper;
 import cz.minesweeper4j.simulation.MinesweeperResult;
 import cz.minesweeper4j.simulation.MinesweeperResult.MinesweeperResultType;
 
 public class Evaluate {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static double test(int runs, int size) {
 		int masterRandomSeed = 10;
-		
-		int size = 40;   // width and height of board
 		
 		int density = 20;  // density percentage
 		
 		int mines = size * size * density / 100;
 		
-		int runs = 10;
 		long totalTime = 0;
 		int totalHints = 0;
 		
@@ -27,8 +22,10 @@ public class Evaluate {
     		MinesweeperResult result = Minesweeper.playAgent(
     				"MyAgent", size, size, mines, 600 * 1000, masterRandomSeed + trial, false, agent);
     		
-    		if (result.getResult() != MinesweeperResultType.VICTORY)
-    			throw new Error("failed");
+    		if (result.getResult() != MinesweeperResultType.VICTORY) {
+                System.out.println("error: failed to solve the level");
+                return -1;
+            }
     		
     		System.out.format("%d x %d, %d mines (%d%% density): solved in %d ms, hints = %d\n",
     		        size, size, mines, density, agent.getThinkTime(), result.getSafeTileSuggestions());
@@ -36,8 +33,14 @@ public class Evaluate {
     		totalTime += agent.getThinkTime();
     		totalHints += result.getSafeTileSuggestions();
 		}
-		
+        
+        double avgHints = 1.0 * totalHints / runs;
 		System.out.format("average over %d runs: time = %d ms, hints = %.1f\n",
-				runs, totalTime / runs, 1.0 * totalHints / runs);
-	}
+                runs, totalTime / runs, avgHints);
+        return avgHints;
+    }
+    
+	public static void main(String[] args) {
+        test(50, 10);
+    }
 }
