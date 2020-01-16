0d573c12e60a88150891c740bf5e3cd5d9edba0c
Adam Dingle <adam@medovina.org>
2020-01-16 13:10:23 +0100

    adjust default board size

--- Minesweeper4J/src/main/java/cz/minesweeper4j/Main.java
+++ Minesweeper4J/src/main/java/cz/minesweeper4j/Main.java
@@ -3,8 +3,7 @@ package cz.minesweeper4j;
 public class Main {
 	
 	public static void main(String[] args) {
-		//Minesweeper.playHuman(10, 10, 10, 10);
-		Minesweeper.playHuman(15, 15, 40, 10);
+		Minesweeper.playHuman(9, 9, 10, 0);
 		
 		// TIMEOUT: 2 minutes
 		//Minesweeper.playHuman(15, 15, 40, 2 * 60 * 1000, 10);
